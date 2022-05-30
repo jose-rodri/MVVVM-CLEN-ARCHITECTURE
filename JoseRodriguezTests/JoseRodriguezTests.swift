f422838c25f52a0256fba14849718298ab8c0d15
Jose Rodriguez <rodriguesmora.001@gmail.com>
2022-05-29 19:32:57 -0500

    Initial Commit

---
+++ JoseRodriguezTests/JoseRodriguezTests.swift
@@ -0,0 +1,33 @@
+//
+//  JoseRodriguezTests.swift
+//  JoseRodriguezTests
+//
+//  Created by Jose Leoncio Quispe Rodriguez on 29/05/22.
+//
+
+import XCTest
+@testable import JoseRodriguez
+
+class JoseRodriguezTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
